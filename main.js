59a9e62ad57bd0f1faee3f233fcefd68578f356d
sukanya10991 <78801848+sukanya10991@users.noreply.github.com>
2021-07-22 19:24:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,28 @@ Webcam.set({
 });
 camera=document.getElementById("camera");
 Webcam.attach( camera );
-function take_snapshot(){
+function takephoto(){
     Webcam.snap(function(data_uri){
    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 console.log('ml5 version:', ml5.version);
-classifier=ml5.imageClassifier('')
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wMNdEbw_x/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+function checkphoto(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(results);
+        document.getElementById("person_name").innerHTML= results[0].label;
+        result=results[0].confidence*100;
+        document.getElementById("person_accuracy").innerHTML=result.toFixed(1)+"%";
+    }
+}
